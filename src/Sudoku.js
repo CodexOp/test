46286de36fcc1b924522761f789e16f952e91df7
Tushar Nagar <66095166+CodexOp@users.noreply.github.com>
2025-06-08 21:17:47 +0530

    Add Sudoku game component

---
+++ src/Sudoku.js
@@ -0,0 +1,13 @@
+// Sudoku game implementation in React
+import React, { useState } from 'react';
+
+const Sudoku = () => {
+  // Implementation code here
+  return (
+    <div>
+      <h1>Sudoku Game</h1>
+    </div>
+  );
+};
+
+export default Sudoku;
